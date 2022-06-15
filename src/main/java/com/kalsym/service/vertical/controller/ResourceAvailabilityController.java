b285d8c5aba290ba9f4d7dcc220800d69a4b9d9e
HasanSajjadSwati <swati.hasansajjad@gmail.com>
2022-06-15 15:50:22 +0500

    Added Cross Origin

--- src/main/java/com/kalsym/service/vertical/controller/ResourceAvailabilityController.java
+++ src/main/java/com/kalsym/service/vertical/controller/ResourceAvailabilityController.java
@@ -36,6 +36,7 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -56,6 +57,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/resources/availabilities/")
+@CrossOrigin("*")
 public class ResourceAvailabilityController {
 
     @Autowired
